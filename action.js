9a95919172b8a7e67ef12c6f751acb2003d4b024
cyssbia <cyssbia@gmail.com>
2018-11-25 09:59:30 +0800

    add myFile Cat

---
+++ action.js
@@ -0,0 +1,26 @@
+/**
+ * Created by hp on 2018/11/24.
+ */
+/*
+$(document).ready(function(){
+    $(".button3").click(function() {
+        $(".button3").css(
+            {
+                width: '120px',
+                height: '46px',
+                backgroundColor: 'white',
+                border: '2px solid brown',
+                color: 'brown'
+            });
+    });
+});
+*/
+
+$(document).ready(function(){
+    $(".button3").click(function(){
+        $(".upPer").animate({
+            top:'-=40px',
+            left:'+=88px'
+        });
+    });
+});
